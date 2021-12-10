6a2c387a5bc72e773f9a585cea939a4493546d5f
Kevin Hirsig <khirsig@2-D-1.42heilbronn.de>
2021-12-10 10:15:40 +0100

    hotfix segfault

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: khirsig <khirsig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 09:15:26 by khirsig           #+#    #+#             */
-/*   Updated: 2021/12/10 10:11:26 by khirsig          ###   ########.fr       */
+/*   Updated: 2021/12/10 10:14:24 by khirsig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Error\n", 2);
 		return (1);
 	}
-	parse_map(&data, argv[1]);
+	if (parse_map(&data, argv[1]) == 0)
+		return (1);
 	index = 0;
 	if (error_map(&data, data.maze.map) == 0)
 		return (1);
