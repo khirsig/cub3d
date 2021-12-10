f8569316600ce204fe5724b226061e8202aa2e54
Jonas Hagedorn <jonas@Jonass-Air.fritz.box>
2021-12-10 11:12:02 +0100

    Add tests

--- main.c
+++ main.c
@@ -21,6 +21,11 @@ int	main(int argc, char **argv)
 	parse_part_1(data.file, &data);
 	printf("Print F: %i, %i, %i\n", data.maze.floor.r, data.maze.floor.g, data.maze.floor.b);
 	printf("Print C: %i, %i, %i\n", data.maze.ceiling.r, data.maze.ceiling.g, data.maze.ceiling.b);
+
+	printf("Print NO path: %s\n", data.maze.north_texture);
+	printf("Print SO path: %s\n", data.maze.south_texture);
+	printf("Print WE path: %s\n", data.maze.west_texture);
+	printf("Print EA path: %s\n", data.maze.east_texture);
 	
 	close(data.file);
 	return (argc);
