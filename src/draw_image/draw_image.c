487e36f57e78cb1a75abf5d301b4b239979a3cbe
Jonas Hagedorn <jhagedor@1-E-13.42heilbronn.de>
2021-12-12 18:02:53 +0100

    Show first view

--- src/draw_image/draw_image.c
+++ src/draw_image/draw_image.c
@@ -6,13 +6,35 @@
 /*   By: jhagedor <jhagedor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 17:57:05 by jhagedor          #+#    #+#             */
-/*   Updated: 2021/12/12 13:13:17 by jhagedor         ###   ########.fr       */
+/*   Updated: 2021/12/12 17:01:34 by jhagedor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
-void	calculate_dist(t_data *data, t_game *game)
+void	my_mlx_pixel_put(t_vars *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void drawVerLine(int i, int drawStart, int drawEnd, t_vars *vars, int side)
+{
+	while (drawStart <= drawEnd)
+	{
+		if (side == 1)
+			my_mlx_pixel_put(vars, i, drawStart, 0x00FF0000);
+		else
+			my_mlx_pixel_put(vars, i, drawStart, 0x00A52A2A);
+		drawStart++;
+	}
+	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->mlx_img, 0, 0);
+}
+
+
+void	calculate_dist(t_data *data, t_game *game, t_vars *vars)
 {
 	int		i;
 	double	cameraX;
@@ -32,17 +54,25 @@ void	calculate_dist(t_data *data, t_game *game)
 	int		stepX;
 	int		stepY;
 	//was there a wall hit?
-	int		hit = 0;
+	int		hit;
 	//was a NS or a EW wall hit?
 	int		side;
+	double	perpWallDist;
 
 	i = 0;
 	while(i < 1000)
 	{
+		if (i < 5)
+		{
+			printf("dirX %f, dirY %f.  ", game->dirX, game->dirY);
+			printf("planeX %f, planeY %f.  ", game->planeX, game->planeY);
+		}
 		cameraX = 2 * i / 1000.0 - 1;
 		rayDirX = game->dirX + game->planeX * cameraX;
 		rayDirY = game->dirY + game->planeY * cameraX;
-		
+
+		if (i < 5)
+			printf("rayDirX %f, rayDirY %f.  ", rayDirX, rayDirY);
 		mapX = game->x_pos;
 		mapY = game->y_pos;
 
@@ -54,6 +84,8 @@ void	calculate_dist(t_data *data, t_game *game)
 			deltaDistY = 1e30;
 		else
 			deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+		if (i < 5)
+			printf("deltaDistX %f, deltaDistY %f.  ", deltaDistX, deltaDistY);
 
 		//calculate step and initial sideDist
 		if(rayDirX < 0)
@@ -77,6 +109,7 @@ void	calculate_dist(t_data *data, t_game *game)
 			sideDistY = (mapY + 1.0 - game->y_pos) * deltaDistY;
 		}
 		//perform DDA
+		hit = 0;
 		while(hit == 0)
 		{
 			//jump to next map square, either in x-direction, or in y-direction
@@ -93,9 +126,29 @@ void	calculate_dist(t_data *data, t_game *game)
 				side = 1;
 			}
 			//Check if ray has hit a wall
-			if(data->maze.map[mapX][mapY] == '1')
+			if(data->maze.map[mapY][mapX] == '1')
 				hit = 1;
 		}
+		//Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
+		if(side == 0)
+			perpWallDist = (sideDistX - deltaDistX);
+		else
+			perpWallDist = (sideDistY - deltaDistY);
+
+		//Calculate height of line to draw on screen
+		int lineHeight = (int)(1000 / perpWallDist);
+
+		//calculate lowest and highest pixel to fill in current stripe
+		int drawStart = -lineHeight / 2 + 1000 / 2;
+		if(drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + 1000 / 2;
+		if(drawEnd >= 1000)
+			drawEnd = 1000 - 1;
+		if (i < 5)
+			printf("This is the distance: %i (Start: %i, End %i)\n", drawEnd - drawStart, drawStart, drawEnd);
+		drawVerLine(i, drawStart, drawEnd, vars, side);
+		i++;
 	}
 }
 
@@ -122,7 +175,7 @@ void	fill_game(t_data *data, t_game *game)
 				game->y_pos = h + 0.5;
 				if (data->maze.map[h][w] == 'N' || data->maze.map[h][w] == 's')
 				{
-					game->planeY = 0.66;
+					game->planeX = 0.66;
 					if (data->maze.map[h][w] == 'N')
 						game->dirY = 1;
 					else
@@ -130,7 +183,7 @@ void	fill_game(t_data *data, t_game *game)
 				}
 				else
 				{
-					game->planeX = 0.66;
+					game->planeY = 0.66;
 					if (data->maze.map[h][w] == 'O')
 						game->dirX = 1;
 					else
