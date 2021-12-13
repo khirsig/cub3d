12841cfeb115090882ab835865680e0edab9e251
Jonas Hagedorn <jhagedor@2-D-3.42heilbronn.de>
2021-12-13 18:03:07 +0100

    Fix view and sprinkle some norminette

--- includes/ray.h
+++ includes/ray.h
@@ -6,7 +6,7 @@
 /*   By: jhagedor <jhagedor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:20:11 by jhagedor          #+#    #+#             */
-/*   Updated: 2021/12/13 15:32:19 by jhagedor         ###   ########.fr       */
+/*   Updated: 2021/12/13 17:27:07 by jhagedor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,17 @@
 
 typedef struct s_ray {
 	double	cameraX;
-	//ray direction
 	double	rayDirX;
 	double	rayDirY;
-	//which box of the map we're in
 	int		mapX;
 	int		mapY;
-	//length of ray from current position to next x or y-side
 	double	sideDistX;
 	double	sideDistY;
-	//length of ray from one x or y-side to next x or y-side
 	double	deltaDistX;
 	double	deltaDistY;
-	//direction to step in x or y-direction (either +1 or -1)
 	int		stepX;
 	int		stepY;
-	//was there a wall hit?
 	int		hit;
-	//was a NS or a EW wall hit?
 	int		side;
 	double	perpWallDist;
 	int		lineHeight;
--- src/draw_image/draw_image.c
+++ src/draw_image/draw_image.c
@@ -6,7 +6,7 @@
 /*   By: jhagedor <jhagedor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 17:57:05 by jhagedor          #+#    #+#             */
-/*   Updated: 2021/12/13 16:20:00 by jhagedor         ###   ########.fr       */
+/*   Updated: 2021/12/13 18:01:20 by jhagedor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,31 @@ void	my_mlx_pixel_put(t_vars *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void drawVerLine(int i, int drawStart, int drawEnd, t_data *data, int side)
+void	draw_ver_line(int i, t_data *data)
 {
-	while (drawStart <= drawEnd)
+	while (data->ray.drawStart <= data->ray.drawEnd)
 	{
-		if (side == 1)
-			my_mlx_pixel_put(&data->vars, i, drawStart, 0x00FF0000);
+		if (data->ray.side == 1)
+			my_mlx_pixel_put(&data->vars, i, data->ray.drawStart, 0x00FF0000);
 		else
-			my_mlx_pixel_put(&data->vars, i, drawStart, 0x00A52A2A);
-		drawStart++;
+			my_mlx_pixel_put(&data->vars, i, data->ray.drawStart, 0x00A52A2A);
+		data->ray.drawStart++;
 	}
 }
 
 /*
-Calculate ray vector (rayDirX, rayDirY) and rounded position of player (mapX, mapY)
+Calculate ray vector (rayDirX, rayDirY)
+and rounded position of player (mapX, mapY)
 */
 void	calculate_ray_vector(t_data *data, int i)
 {
-		data->ray.cameraX = 2 * i / 1000.0 - 1;
-		data->ray.rayDirX = data->player.dirX + data->player.planeX * data->ray.cameraX;
-		data->ray.rayDirY = data->player.dirY + data->player.planeY * data->ray.cameraX;
-		data->ray.mapX = data->player.x_pos;
-		data->ray.mapY = data->player.y_pos;
+	data->ray.cameraX = 2 * i / 1000.0 - 1;
+	data->ray.rayDirX = data->player.dirX
+		+ data->player.planeX * data->ray.cameraX;
+	data->ray.rayDirY = data->player.dirY
+		+ data->player.planeY * data->ray.cameraX;
+	data->ray.mapX = data->player.x_pos;
+	data->ray.mapY = data->player.y_pos;
 }
 
 /*
@@ -52,51 +55,55 @@ void	calculate_delta_dist(t_data *data)
 	if (data->ray.rayDirX == 0)
 		data->ray.deltaDistX = 1e30;
 	else
-		data->ray.deltaDistX = sqrt(1 + (data->ray.rayDirY * data->ray.rayDirY) / (data->ray.rayDirX * data->ray.rayDirX));
+		data->ray.deltaDistX = sqrt(1 + (data->ray.rayDirY * data->ray.rayDirY)
+				/ (data->ray.rayDirX * data->ray.rayDirX));
 	if (data->ray.rayDirY == 0)
 		data->ray.deltaDistY = 1e30;
 	else
-		data->ray.deltaDistY = sqrt(1 + (data->ray.rayDirX * data->ray.rayDirX) / (data->ray.rayDirY * data->ray.rayDirY));
+		data->ray.deltaDistY = sqrt(1 + (data->ray.rayDirX * data->ray.rayDirX)
+				/ (data->ray.rayDirY * data->ray.rayDirY));
 }
 
 /*
-calculate step and initial sideDist
+Calculate step and initial sideDist
 */
 void	calculate_step_and_sideDist(t_data *data)
 {
-	if(data->ray.rayDirX < 0)
+	if (data->ray.rayDirX < 0)
 	{
 		data->ray.stepX = -1;
-		data->ray.sideDistX = (data->player.x_pos - data->ray.mapX) * data->ray.deltaDistX;
+		data->ray.sideDistX = (data->player.x_pos - data->ray.mapX)
+			* data->ray.deltaDistX;
 	}
 	else
 	{
 		data->ray.stepX = 1;
-		data->ray.sideDistX = (data->ray.mapX + 1.0 - data->player.x_pos) * data->ray.deltaDistX;
+		data->ray.sideDistX = (data->ray.mapX + 1.0 - data->player.x_pos)
+			* data->ray.deltaDistX;
 	}
-	if(data->ray.rayDirY < 0)
+	if (data->ray.rayDirY < 0)
 	{
-		data->ray.stepY = -1;
-		data->ray.sideDistY = (data->player.y_pos - data->ray.mapY) * data->ray.deltaDistY;
+		data->ray.stepY = 1;
+		data->ray.sideDistY = (data->player.y_pos - data->ray.mapY)
+			* data->ray.deltaDistY;
 	}
 	else
 	{
-		data->ray.stepY = 1;
-		data->ray.sideDistY = (data->ray.mapY + 1.0 - data->player.y_pos) * data->ray.deltaDistY;
+		data->ray.stepY = -1;
+		data->ray.sideDistY = (data->ray.mapY + 1.0 - data->player.y_pos)
+			* data->ray.deltaDistY;
 	}
 }
 
 /*
-calculate step and initial sideDist
+Calculate step and initial sideDist
 */
 void	perform_DDA(t_data *data)
 {
-	//perform DDA
 	data->ray.hit = 0;
-	while(data->ray.hit == 0)
+	while (data->ray.hit == 0)
 	{
-		//jump to next map square, either in x-direction, or in y-direction
-		if(data->ray.sideDistX < data->ray.sideDistY)
+		if (data->ray.sideDistX < data->ray.sideDistY)
 		{
 			data->ray.sideDistX += data->ray.deltaDistX;
 			data->ray.mapX += data->ray.stepX;
@@ -108,96 +115,99 @@ void	perform_DDA(t_data *data)
 			data->ray.mapY += data->ray.stepY;
 			data->ray.side = 1;
 		}
-		//Check if ray has hit a wall
-		if(data->maze.map[data->ray.mapY][data->ray.mapX] == '1')
+		if (data->maze.map[data->ray.mapY][data->ray.mapX] == '1')
 			data->ray.hit = 1;
 	}
 }
 
 /*
-calculate ray distance and hight
+Calculate ray distance and hight
 */
 void	calc_ray_dist(t_data *data)
 {
-	//Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
-	if(data->ray.side == 0)
+	if (data->ray.side == 0)
 		data->ray.perpWallDist = (data->ray.sideDistX - data->ray.deltaDistX);
 	else
 		data->ray.perpWallDist = (data->ray.sideDistY - data->ray.deltaDistY);
-
-	//Calculate height of line to draw on screen
 	data->ray.lineHeight = (int)(1000 / data->ray.perpWallDist);
-
-	//calculate lowest and highest pixel to fill in current stripe
-	data->ray.drawStart = - data->ray.lineHeight / 2 + 1000 / 2;
-	if(data->ray.drawStart < 0)
+	data->ray.drawStart = 1000 / 2 - data->ray.lineHeight / 2;
+	if (data->ray.drawStart < 0)
 		data->ray.drawStart = 0;
 	data->ray.drawEnd = data->ray.lineHeight / 2 + 1000 / 2;
-	if(data->ray.drawEnd >= 1000)
+	if (data->ray.drawEnd >= 1000)
 		data->ray.drawEnd = 1000 - 1;
-	// if (i < 5)
-	// 	printf("This is the distance: %i (Start: %i, End %i)\n", data->ray.drawEnd - data->ray.drawStart, data->ray.drawStart, data->ray.drawEnd);
 }
 
+/*
+Draw player view.
+1) Calculate the ray vector (x and y value) for each vertical line.
+2) Prepare for DDA algorithm.
+3) Find the wall with DDA algorithm.
+4) Calculate distance to wall.
+5) Draw line accordingly.
+*/
 void	draw_view(t_data *data)
 {
 	int		i;
 
 	i = 0;
-	while(i < 1000)
+	while (i < 1000)
 	{
 		calculate_ray_vector(data, i);
 		calculate_delta_dist(data);
 		calculate_step_and_sideDist(data);
 		perform_DDA(data);
 		calc_ray_dist(data);
-		drawVerLine(i, data->ray.drawStart, data->ray.drawEnd, data, data->ray.side);
+		draw_ver_line(i, data);
 		i++;
 	}
 }
 
+/*
+Helper function
+*/
+void	fill_player_helper(t_data *data, int h, int w)
+{
+	if (data->maze.map[h][w] != '0' && data->maze.map[h][w] != '1')
+	{
+		data->player.x_pos = w + 0.5;
+		data->player.y_pos = h + 0.5;
+		if (data->maze.map[h][w] == 'N' || data->maze.map[h][w] == 'S')
+		{
+			data->player.planeX = 0.66;
+			if (data->maze.map[h][w] == 'N')
+				data->player.dirY = 1;
+			else
+				data->player.dirY = -1;
+		}
+		else
+		{
+			data->player.planeY = 0.66;
+			if (data->maze.map[h][w] == 'E')
+				data->player.dirX = 1;
+			else
+				data->player.dirX = -1;
+		}
+	}
+}
 
 /*
 Loops through map and fills position, direction and plane.
 */
-
 void	fill_player(t_data *data)
 {
 	int	w;
 	int	h;
 
 	h = 0;
-	while(data->maze.map[h])
+	while (data->maze.map[h])
 	{
 		w = 0;
 		while (data->maze.map[h][w])
 		{
-			if (data->maze.map[h][w] == 'N' || data->maze.map[h][w] == 'S'
-				|| data->maze.map[h][w] == 'W' || data->maze.map[h][w] == 'E')
-			{
-				printf("PPos: %i %i\n", h, w);
-				data->player.x_pos = w + 0.5;
-				data->player.y_pos = h + 0.5;
-				if (data->maze.map[h][w] == 'N' || data->maze.map[h][w] == 'S')
-				{
-					data->player.planeX = 0.66;
-					if (data->maze.map[h][w] == 'N')
-						data->player.dirY = 1;
-					else
-						data->player.dirY = -1;
-				}
-				else
-				{
-					data->player.planeY = 0.66;
-					if (data->maze.map[h][w] == 'E')
-						data->player.dirX = 1;
-					else
-						data->player.dirX = -1;
-				}
-			}
+			fill_player_helper(data, h, w);
 			w++;
 		}
 		h++;
 	}
-
 }
